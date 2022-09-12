fef96c27636279e8cb86865a564d7c3cd6f201ed
NatanelMunoz <natanael.munoz.s@gmail.com>
2022-09-12 08:49:11 -0500

    Inicio de avances del proyecto final

---
+++ controller/LoginController.php
@@ -0,0 +1,88 @@
+<?php
+
+require_once 'model/dao/LoginDAO.php';
+require_once 'model/dto/Login.php';
+
+class LoginController extends Controller{
+
+    private $model;
+
+    function __construct(){
+        parent::__construct();
+        $this->model = new LoginDAO();
+    }
+    
+    function buscarProductos(){
+        $resultados = $this->model->buscarProductos();
+        $this->view->setResultados($resultados);
+        $this->view->mostrarVista('Producto/Buscar');
+    }
+    
+    function buscarProductoPorNombre(){
+        $parametro = (!empty($_POST["b"]))?htmlentities($_POST["b"]):"";
+        if(!empty($parametro)){
+            $resultados = $this->model->buscarPorNombre($parametro);
+        }else{
+            $resultados = $this->model->buscarProductos();
+        }
+        $this->view->setResultados($resultados);
+        $this->view->mostrarVista('Producto/Buscar');
+    }
+
+    public function editarVista(){
+        $id= $_REQUEST['id']; 
+        $prod = $this->model->buscarPorId($id);
+        $this->view->setResultados($prod);
+        $this->view->mostrarEdicion('Producto/Editar');
+     }
+
+     public function editarProducto(){
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $producto = new Producto();
+            $producto->setPrd_id(($_POST['id']));
+            $producto->setPrd_nombre(($_POST['nombre']));
+            $producto->setPrd_valor(($_POST['valor']));
+            $producto->setPrd_cantidad(($_POST['cantidad']));
+
+            $estado = (isset($_POST['estado'])) ? 1 : 0;
+            $producto->setPrd_estado($estado);
+            $producto->setPrd_codigo_proveedor_producto(($_POST['proveedor']));
+            
+            $fechaActual = new DateTime('NOW');
+            $producto->setPrd_fecha_actualizacion($fechaActual->format('Y-m-d H:i:s'));
+            
+            $this->model->actualizar($producto);
+
+            $resultados = $this->model->buscarProductos();
+            $this->view->setResultados($resultados);
+            $this->view->mostrarVista('Producto/Buscar');
+        }
+     }
+
+     public function nuevo(){
+        $this->view->mostrarVista('Producto/Nuevo');
+     }
+
+     public function nuevoProducto(){
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $prod = new Producto();
+            $prod->setPrd_nombre(htmlentities($_POST['nombre']));
+            $prod->setPrd_valor(htmlentities($_POST['valor']));
+            $prod->setPrd_cantidad(htmlentities($_POST['cantidad']));
+            $prod->setPrd_codigo_proveedor_producto(htmlentities($_POST['proveedor']));
+
+            $estado = (isset($_POST['estado'])) ? 1 : 0;
+            $prod->setPrd_estado($estado);
+           
+            $fechaActual = new DateTime('NOW');
+            $prod->setPrd_fecha_actualizacion($fechaActual->format('Y-m-d H:i:s'));
+            
+            $this->model->insertarProducto($prod);
+
+            $resultados = $this->model->buscarProductos();
+            $this->view->setResultados($resultados);
+            $this->view->mostrarVista('Producto/Buscar');
+        } 
+     }
+}
+?>
---
+++ model/dto/Usuarios.php
@@ -0,0 +1,59 @@
+<?php
+
+class Usuarios{
+
+    private $srs_id,$srs_nombre_usuario, $srs_clave, $srs_rol_fk;
+
+    function __construct() {}
+
+    public function getSrs_id(){
+        return $this->srs_id;
+    }
+
+    public function setSrs_id($id){
+        $this->srs_id = $id;
+    }
+
+    public function getSrs_nombre_usuario(){
+        return $this->srs_nombre_usuario;
+    }
+
+    public function setSrs_nombre_usuario($nombre){
+        $this->srs_nombre_usuario = $nombre;
+    }
+
+    public function getSrs_clave(){
+        return $this->srs_clave;
+    }
+
+    public function setSrs_clave($clave){
+        $this->srs_clave = $clave;
+    }
+
+    public function getSrs_rol_fk(){
+        return $this->srs_rol_fk;
+    }
+
+    public function setSrs_rol_fk($rol){
+        $this->srs_rol_fk = $rol;
+    }
+
+    public function __set($nombre, $valor) {
+        if (property_exists('Usuario', $nombre)) {
+            $this->$nombre = $valor;
+        } else {
+            echo $nombre . "No existe..";
+        }
+    }
+
+    public function __get($nombre) {
+        if (property_exists('Usuario', $nombre)) {
+            return $this->$nombre;
+        }
+        return NULL;
+    }
+
+    
+}
+
+?>
