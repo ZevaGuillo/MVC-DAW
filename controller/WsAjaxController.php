58754262b37a21b5b818ec876999f5b82a1a53dd
NatanelMunoz <natanael.munoz.s@gmail.com>
2022-09-14 13:42:48 -0500

    Inicio AJAX

---
+++ controller/WsAjaxController.php
@@ -0,0 +1,22 @@
+<?php
+// AUTOR: MUÑOZ SOLORZANO JOHANAN NATANAEL
+class WsAjaxController{
+
+    private $model;
+
+    function __construct(){
+        $this->model = new ProductoDAO();
+    }
+
+    //CONSULTA PARA TRAER PROVEEDORES DE LA DB Y POSTERIORMENTE AGREGAR A UN COMBOBOX
+    public function consultarProveedores(){
+        try {
+
+        }catch(Exception $e){
+
+        }
+    }
+
+}
+?>
+
