3acb330acee0d8dedcb678530cff37d9c430caea
JoeTech-Studio <joevelez_1234@outlook.es>
2022-09-18 19:00:03 -0500

    a

--- controller/PortadaController.php
+++
@@ -1,9 +0,0 @@
-<?php
-// autor:MUÑOZ SOLORZANO JOHANAN NATANAEL
-
-require_once 'config/config.php';
-class PortadaController extends Controller{
-    function presentar(){        
-        $this->view->mostrarVista('portada/presentacion');
-    }
-}
